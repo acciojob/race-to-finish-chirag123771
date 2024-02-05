9a17ad034d8a03879e063bf046d91658dcd2eb76
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-02-06 00:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 window.promises = [];
+ new Promise(resolve => setTimeout(() => resolve('Promise 1'), Math.random() * 5000)),
+      new Promise(resolve => setTimeout(() => resolve('Promise 2'), Math.random() * 5000)),
+      new Promise(resolve => setTimeout(() => resolve('Promise 3'), Math.random() * 5000)),
+      new Promise(resolve => setTimeout(() => resolve('Promise 4'), Math.random() * 5000)),
+      new Promise(resolve => setTimeout(() => resolve('Promise 5'), Math.random() * 5000)),
+    ];
 
+    Promise.any(promises)
+      .then(result => {
+        document.getElementById('output').innerText = result;
+      })
+      .catch(error => {
+        console.error(error);
+      });
 // Do not change the code above this
 // add your promises to the array `promises`
